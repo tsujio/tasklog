0b498e6a5e55c53ed1ff84397c3b39e1db0bea5a
tsujio <ntsujio@gmail.com>
2015-06-18 23:24:55 +0900

    Introduce task state

--- tasklog.pm
+++ tasklog.pm
@@ -27,6 +27,28 @@ sub get_db_file_path {
   $db_file_path;
 }
 
+# Task states
+my %state_ids = (
+  INITIAL => 0,
+  SUSPENDED => 1,
+  ACTIVE => 2,
+  BLOCKED => 3,
+  CLOSED => 4,
+);
+
+# Convert state string to number
+sub str2stateid {
+  die "Unexpected state string." unless exists($state_ids{$_[0]});
+  $state_ids{$_[0]};
+}
+
+# Convert state number to string
+sub stateid2str {
+  my %rev = reverse %state_ids;
+  die "Unexpected state number." unless exists($rev{$_[0]});
+  $rev{$_[0]};
+}
+
 # Check if list contains given string
 sub contain {
   List::Util::any { $_ eq $_[1] } @{$_[0]};
@@ -79,6 +101,22 @@ sub str2datetime {
   return sprintf 'datetime("%s")', $dt->strftime($DATETIME_FORMAT);
 }
 
+# Convert utc to localtime string
+sub utc2localtime {
+  my $utc_str = shift;
+
+  # Parse datetime string
+  my $strp = DateTime::Format::Strptime->new(
+    pattern => $DATETIME_FORMAT, time_zone => 'UTC');
+  my $dt = $strp->parse_datetime($utc_str);
+
+  # Convert timezone
+  $dt->set_time_zone($TIME_ZONE);
+
+  # Convert to string
+  $dt->strftime($DATETIME_FORMAT);
+}
+
 # Invoke given function with DB connection
 sub invoke_with_connection {
   my ($opt, $func) = @_ > 1 ? ($_[0], $_[1]) : ('', $_[0]);
@@ -116,13 +154,45 @@ sub task_exists {
   $sth->fetchrow_hashref
 }
 
-# Check if logs of given task exist
+# Check if activities of given task exist
 sub task_log_exists {
   my $dbh = shift;
   my $task_name = shift;
-  my $sth = $dbh->prepare('SELECT * FROM logs WHERE task_name = ?');
+  my $sth = $dbh->prepare('SELECT * FROM activities WHERE task_name = ?');
   $sth->execute($task_name);
   $sth->fetchrow_hashref
+}
+
+# Get current task name
+sub get_current_task {
+  my $dbh = shift;
+  my $rows = $dbh->selectall_arrayref(
+    "SELECT task_name FROM activities WHERE end_utc = $DATETIME_INF");
+  die "There are multiple active tasks." if @$rows > 1;
+  @$rows > 0 ? $rows->[0][0] : undef;
+}
+
+# Switch task state
+sub switch_task_state {
+  my $dbh = shift;
+  my ($task_name, $state, $datetime) = @_;
+  $datetime //= $DATETIME_NOW;
+
+  # Get state of given task
+  my $sth = $dbh->prepare('SELECT name, state FROM tasks WHERE name = ?');
+  $sth->execute($task_name);
+  my $task = $sth->fetchrow_hashref;
+  die "Cannot determine which task to change state." if $sth->fetchrow_hashref;
+
+  # Record task state history
+  $sth = $dbh->prepare(
+    "INSERT INTO task_state_history(task_name, state, until_utc) " .
+    "VALUES(?, ?, $datetime);");
+  $sth->execute($task->{name}, $task->{state});
+
+  # Update task state
+  $sth = $dbh->prepare('UPDATE tasks SET state = ? WHERE name = ?;');
+  $sth->execute(str2stateid($state), $task_name);
 }
 
 # Execute start command
@@ -140,16 +210,17 @@ sub execute_start {
     # Verify that task to start exists
     die "Task $task_name not found." unless task_exists($dbh, $task_name);
 
-    # Raise error if active tasks exist
-    my $cnt = $dbh->selectall_arrayref(
-      "SELECT count(*) FROM logs WHERE end_utc = $DATETIME_INF");
-    die "$cnt->[0][0] active tasks already exist." if $cnt->[0][0] > 0;
+    # Verify that no active task exists
+    die "active task already exist." if get_current_task($dbh);
 
     # Start task
     my $sth = $dbh->prepare(
-      "INSERT INTO logs(task_name, start_utc, end_utc) " .
+      "INSERT INTO activities(task_name, start_utc, end_utc) " .
         "VALUES(?, $start_datetime, $DATETIME_INF);");
     $sth->execute($task_name);
+
+    # Switch task state to active
+    switch_task_state($dbh, $task_name, 'ACTIVE', $start_datetime);
 
     say "Task $task_name started.";
   };
@@ -165,16 +236,18 @@ sub execute_end {
   invoke_with_connection sub {
     my $dbh = shift;
 
-    # Raise error if multiple active tasks exist
-    my $active_tasks = $dbh->selectall_arrayref(
-      "SELECT task_name FROM logs WHERE end_utc = $DATETIME_INF");
-    die "Cannot determine which task to end." if @$active_tasks != 1;
+    # Verify that task to end exists
+    my $task_name = get_current_task($dbh);
+    die "Cannot determine which task to end." unless $task_name;
 
     # End task
-    $dbh->do("UPDATE logs SET end_utc = $end_datetime " .
+    $dbh->do("UPDATE activities SET end_utc = $end_datetime " .
                "WHERE end_utc = $DATETIME_INF;");
 
-    say "Task $active_tasks->[0][0] ended.";
+    # Switch task state to suspended
+    switch_task_state($dbh, $task_name, 'SUSPENDED', $end_datetime);
+
+    say "Task $task_name ended.";
   };
 }
 
@@ -189,19 +262,22 @@ sub execute_switch {
     # Verify that task to start exists
     die "Task $task_name not found." unless task_exists($dbh, $task_name);
 
-    # Raise error if multiple active tasks exist
-    my $active_tasks = $dbh->selectall_arrayref(
-      "SELECT task_name FROM logs WHERE end_utc = $DATETIME_INF");
-    die "Cannot determine which task to switch." if @$active_tasks != 1;
+    # Verify that task to end exists
+    my $old_task = get_current_task($dbh);
+    die "Cannot determine which task to switch." unless $old_task;
 
     # Switch task
-    $dbh->do("UPDATE logs SET end_utc = $DATETIME_NOW " .
+    $dbh->do("UPDATE activities SET end_utc = $DATETIME_NOW " .
                "WHERE end_utc = $DATETIME_INF;");
-    my $sth = $dbh->prepare("INSERT INTO logs(task_name, start_utc, end_utc) " .
+    my $sth = $dbh->prepare("INSERT INTO activities(task_name, start_utc, end_utc) " .
                               "VALUES(?, $DATETIME_NOW, $DATETIME_INF);");
     $sth->execute($task_name);
 
-    say "Task switched: $active_tasks->[0][0] -> $task_name";
+    # Change task states
+    switch_task_state($dbh, $old_task, 'SUSPENDED');
+    switch_task_state($dbh, $task_name, 'ACTIVE');
+
+    say "Task switched: $old_task -> $task_name";
   };
 }
 
@@ -209,29 +285,18 @@ sub execute_switch {
 sub execute_show {
   invoke_with_connection 'readonly', sub {
     my $dbh = shift;
-    my $sth = $dbh->prepare('SELECT * FROM logs ORDER BY start_utc');
+    my $sth = $dbh->prepare('SELECT * FROM activities ORDER BY start_utc');
     $sth->execute;
 
-    # Show logs
+    # Show activities
     say "id\ttask_name\tstart\tend";
     say "-" x 79;
-    my $strp = DateTime::Format::Strptime->new(
-      pattern => $DATETIME_FORMAT, time_zone => 'UTC');
     while (my $row = $sth->fetchrow_hashref) {
-      # Parse datetime string
-      my $start_dt = $strp->parse_datetime($row->{start_utc});
-      my $end_dt = $strp->parse_datetime($row->{end_utc});
-
-      # Convert timezone
-      $start_dt->set_time_zone($TIME_ZONE);
-      $end_dt->set_time_zone($TIME_ZONE);
-
-      # Print
       printf "%s\t%s\t%s\t%s\n",
         $row->{id}, $row->{task_name},
-        $start_dt->strftime($DATETIME_FORMAT),
+        utc2localtime($row->{start_utc}),
         $row->{end_utc} ne $DATETIME_INF_STR ?
-          $end_dt->strftime($DATETIME_FORMAT) : '';
+          utc2localtime($row->{end_utc}) : '';
     }
   };
 }
@@ -240,10 +305,10 @@ sub execute_show {
 sub execute_task {
   my $arg = shift;
   my $task_name = shift;
-  if (not $arg or not contain ['add', 'remove', 'list'], $arg) {
+  if (not $arg or not contain ['add', 'remove', 'list', 'history'], $arg) {
     die "Unexpected args for 'task'";
   }
-  if (($arg eq 'add' or $arg eq 'remove') and not $task_name) {
+  if (contain ['add', 'remove', 'history'], $arg and not $task_name) {
     die "Task name must be specified.";
   }
 
@@ -252,8 +317,9 @@ sub execute_task {
     invoke_with_connection sub {
       my $dbh = shift;
       die "Task $task_name already exists." if task_exists($dbh, $task_name);
-      my $sth = $dbh->prepare('INSERT INTO tasks(name) VALUES(?);');
-      $sth->execute($task_name);
+      my $sth = $dbh->prepare(
+        "INSERT INTO tasks(name, state, created_at) VALUES(?, ?, $DATETIME_NOW);");
+      $sth->execute($task_name, str2stateid('INITIAL'));
       say "Added new task: $task_name";
     };
   } elsif ($arg eq 'remove') {
@@ -261,7 +327,7 @@ sub execute_task {
     invoke_with_connection sub {
       my $dbh = shift;
       die "Task $task_name not found." unless task_exists($dbh, $task_name);
-      die "Cannot delete task $task_name because some logs have been recorded."
+      die "Cannot delete task $task_name because some activities have been recorded."
         if task_log_exists($dbh, $task_name);
       my $sth = $dbh->prepare('DELETE FROM tasks WHERE name = ?');
       $sth->execute($task_name);
@@ -273,9 +339,34 @@ sub execute_task {
       my $dbh = shift;
       my $sth = $dbh->prepare('SELECT * FROM tasks;');
       $sth->execute();
+      say "name\tstate\tcreated_at";
+      say '-' x 79;
       while (my $row = $sth->fetchrow_hashref) {
-        say $row->{name};
+        say sprintf "%s\t%s\t%s",
+          $row->{name}, stateid2str($row->{state}),
+          utc2localtime($row->{created_at});
       }
+    };
+  } elsif ($arg eq 'history') {
+    # Show task history
+    invoke_with_connection 'readonly', sub {
+      my $dbh = shift;
+      die "Task $task_name not found." unless task_exists($dbh, $task_name);
+      my $sth = $dbh->prepare(
+        'SELECT * FROM task_state_history WHERE task_name = ? ' .
+        'ORDER BY until_utc;');
+      $sth->execute($task_name);
+      say "state\tuntil";
+      say '-' x 79;
+      while (my $row = $sth->fetchrow_hashref) {
+        say sprintf "%s\t%s",
+          stateid2str($row->{state}),
+          utc2localtime($row->{until_utc});
+      }
+      $sth = $dbh->prepare('SELECT state FROM tasks WHERE name = ?');
+      $sth->execute($task_name);
+      my $current_state = $sth->fetchrow_hashref()->{state};
+      say sprintf "%s\t", stateid2str($current_state);
     };
   }
 }
@@ -292,7 +383,7 @@ sub execute_db {
     invoke_with_connection 'create', sub {
       my $dbh = shift;
       $dbh->do(<<SQL);
-CREATE TABLE logs (
+CREATE TABLE activities (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   task_name CHAR(64) REFERENCES tasks(name) NOT NULL,
   start_utc DATETIME NOT NULL,
@@ -301,7 +392,17 @@ CREATE TABLE logs (
 SQL
       $dbh->do(<<SQL);
 CREATE TABLE tasks (
-  name CHAR(64) PRIMARY KEY
+  name CHAR(64) PRIMARY KEY,
+  state INT UNSIGNED NOT NULL,
+  created_at DATETIME NOT NULL
+);
+SQL
+      $dbh->do(<<SQL);
+CREATE TABLE task_state_history (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  task_name CHAR(64) REFERENCES tasks(name) NOT NULL,
+  state INT UNSIGNED NOT NULL,
+  until_utc DATETIME NOT NULL
 );
 SQL
       $dbh->do(<<SQL);
@@ -311,7 +412,7 @@ CREATE TABLE config (
 );
 SQL
       $dbh->do(
-        'INSERT INTO config(name, value) VALUES("version", "0.1.0");');
+        'INSERT INTO config(name, value) VALUES("version", "0.2.0");');
     };
   } elsif ($arg eq 'desc') {
     # Show DB description
@@ -339,7 +440,7 @@ COMMANDS:
   end [[yyyy-MM-dd] hh:mm[:ss]]
   switch TASK       (alias: s)
   show
-  task (add TASK| remove TASK| list)
+  task (add TASK | remove TASK | list | history TASK)
   db (setup | desc)
   help
 EOS
